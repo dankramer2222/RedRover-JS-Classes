d0a07cd38bdc1cfe0eeb9f79c7ea23c521a36f66
Danila Albutov <danil.miheev2222@gmail.com>
2024-03-07 14:10:33 -0800

    lecture 5 objects

--- HomeTasksLaura/TaskFromLiveStreamLaura/lecture5.js
+++ HomeTasksLaura/TaskFromLiveStreamLaura/lecture5.js
@@ -3,3 +3,34 @@ const arr2 = arr1;
 arr1.push('a');
 console.log(arr1);
 console.log(arr2);
+
+let obj = {};
+const guitar = {};
+
+console.log(typeof obj);
+
+obj = 'hello';
+
+console.log(typeof obj);
+
+// guitar = 15 
+// console.log(typeof guitar); //erorr 
+
+const meta ={
+    for:1,
+    return:2,
+    let:3,
+}
+console.log(meta.for + meta.return + meta.let);
+
+const personGirl = new Object;
+
+personGirl.feet = 2;
+personGirl.name = 'Sandy';
+personGirl.hair = 'black';
+
+console.log(personGirl)
+console.log("------------------------")
+let person = {}
+person.firstName = "Bob"
+console.log(person);
