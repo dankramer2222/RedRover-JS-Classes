238563ca9b7982d35720d1642dfdecfc96754d66
Danila Albutov <danil.miheev2222@gmail.com>
2024-03-20 14:47:19 -0700

    3rd live code alex lesson

---
+++ AlexClasses/less3.js
@@ -0,0 +1,34 @@
+// number
+let tnum = 25;
+let tfloat = 3.14
+
+//string
+
+let names = 'dan'
+let postalCode = '98001'
+
+// boolean
+let knowledgeJS = true
+let sleep = false
+
+// this variable is declared
+let undefined_variable;
+var a
+// const b => error without declaration
+
+console.log(undefined_variable);
+console.log(a);
+
+//null 
+let this_is_null = null
+console.log(this_is_null);
+console.log("*______________________________________________________*")
+console.log(typeof tnum)
+console.log(typeof tfloat)
+console.log(typeof names)
+console.log(typeof postalCode)
+console.log(typeof knowledgeJS) 
+console.log(typeof sleep)
+console.log(typeof undefined_variable)
+console.log(typeof a)
+console.log(typeof null)
