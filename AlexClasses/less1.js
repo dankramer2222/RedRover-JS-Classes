0976c04cf9104bb71d146e2462651e1289cc2e0f
Danila Albutov <danil.miheev2222@gmail.com>
2024-03-19 23:14:17 -0700

    add folder with alex classes

---
+++ AlexClasses/less1.js
@@ -0,0 +1,9 @@
+console.log("classes js from alex!");
+
+let user_name = 'dan'
+
+//concatenation
+console.log(user_name + " will be attendant in this classes");
+
+//interpolation
+console.log(`hello ${user_name}`);
