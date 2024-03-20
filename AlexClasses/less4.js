14e2efaa44458a639bcf063151af6096fecfab41
Danila Albutov <danil.miheev2222@gmail.com>
2024-03-20 14:55:16 -0700

    live code less4 alex

---
+++ AlexClasses/less4.js
@@ -0,0 +1,33 @@
+// comparison operator js
+
+//equal
+
+let isEqual = 5 == "5"
+console.log(isEqual);
+
+//strict equal
+
+let isStrictEqual = 5 === '5'
+console.log(isStrictEqual);
+
+//not equal
+let isNotEqual = 5 != 8
+console.log(isNotEqual);
+
+// strict not equal
+let strictNotEqual = 5 !== '5'
+console.log(strictNotEqual);
+
+//greater then
+let greaterN = 5 > 8 
+console.log(greaterN);
+
+let lessT = 5 < 8
+console.log(lessT);
+
+//greater or equal
+let greatOrEqual = 5 >=5
+console.log(greatOrEqual);
+//less or equal
+let lessOrEqual = 5<= 4
+console.log(lessOrEqual);
